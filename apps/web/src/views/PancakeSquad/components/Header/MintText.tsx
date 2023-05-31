2b5b159654171eaa6ca4ed11a4021bed20b4bbd0
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 18:23:44 +0700

    Update MintText.tsx
    コードにはフロントエンドのテキスト、カラーに関係するコードが含まれています。具体的には、Flex、lightColors、Text、ContextApi、SaleStatusEnum、UserStatusEnumなどのコードが含まれています。これらのコードは、フロントエンドのテキストとカラーを表示するために使用されます。
    
    また、このコードは、ユーザーがNFTを請求できるかどうかを表示しています。ユーザーがNFTを請求できる場合、ユーザーは「Redeem Tickets to claim NFTs」というテキストが表示されます。ユーザーがNFTを請求できない場合、ユーザーは「Sorry, you can’t claim any NFTs! Better luck next time.」というテキストが表示されます。

--- apps/web/src/views/PancakeSquad/components/Header/MintText.tsx
+++ apps/web/src/views/PancakeSquad/components/Header/MintText.tsx
@@ -1,5 +1,5 @@
-import { Flex, lightColors, Text } from '@pancakeswap/uikit'
-import { ContextApi } from '@pancakeswap/localization'
+import { Flex, lightColors, Text } from '@xoxoswap/uikit'
+import { ContextApi } from '@xoxoswap/localization'
 
 import { SaleStatusEnum, UserStatusEnum } from '../../types'
 
