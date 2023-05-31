ec719e63e1fe695cec99c047d728700e896fe540
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 16:49:07 +0700

    Update styleds.ts
    SwapShowAcceptChanges コンポーネントの background-color プロパティは、背景色をテーマの colors.warning33 プロパティで指定された色に設定します。
    TruncatedText コンポーネントの width プロパティは、テキストの幅を 220px に設定します。
    これらのコードは、フロントエンドのテキストと色の外観を制御するために使用されます。

--- apps/aptos/components/Swap/styleds.ts
+++ apps/aptos/components/Swap/styleds.ts
@@ -1,6 +1,7 @@
 import { AutoColumn, Text } from '@pancakeswap/uikit'
 import styled from 'styled-components'
 
+
 export const SwapShowAcceptChanges = styled(AutoColumn)`
   background-color: ${({ theme }) => `${theme.colors.warning33}`};
   padding: 0.5rem;
