16e6d56286a0f7acd73da95b5058c0ed82a3ac54
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 18:06:08 +0700

    Update PreTitle.tsx
    このコードにはフロントエンドのテキストとカラーに関係するコードが含まれています。具体的には、colorプロパティ、fontSizeプロパティ、boldプロパティ、textTransformプロパティです。colorプロパティは、テキストの色を指定します。fontSizeプロパティは、テキストのフォントサイズを指定します。boldプロパティは、テキストを太字にします。textTransformプロパティは、テキストを大文字にします。これらのコードは、フロントエンドのテキストとカラーを変更するために使用できます。
    
    PreTitleコンポーネントは、見出しの前に表示されるテキストを表示するためのコンポーネントです。このコンポーネントは、Textコンポーネントをインポートして使用しています。Textコンポーネントは、テキストを表示するためのコンポーネントです。このコンポーネントには、colorプロパティ、fontSizeプロパティ、boldプロパティ、textTransformプロパティなどのプロパティが用意されています。これらのプロパティを使用して、テキストの色、フォントサイズ、太字、大文字を変更することができます。

--- packages/uikit/src/components/Text/PreTitle.tsx
+++ packages/uikit/src/components/Text/PreTitle.tsx
@@ -3,6 +3,7 @@ import Text from "./Text";
 import { TextProps } from "./types";
 
 
+
 export const PreTitle = (props: PropsWithChildren<TextProps>) => (
   <Text color="secondary" fontSize="12px" bold textTransform="uppercase" {...props} />
 );
