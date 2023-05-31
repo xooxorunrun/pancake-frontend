86ddf58a5ae459c6b8488a7a35facc5c83248ac7
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 18:11:56 +0700

    Update text.test.tsx
    コードにはフロントエンドのテキストとカラーに関連するコードが含まれています。コードの<Text>タグは、テキストをレンダリングするために使用されます。コードのcolor属性は、テキストの色を設定するために使用されます。コードのfont-size属性は、テキストのサイズを設定するために使用されます。

--- packages/uikit/src/__tests__/components/text.test.tsx
+++ packages/uikit/src/__tests__/components/text.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { renderWithProvider } from "../../testHelpers";
 import Text from "../../components/Text/Text";
 
+
 it("renders correctly", () => {
   const { asFragment } = renderWithProvider(<Text>pancake</Text>);
   expect(asFragment()).toMatchInlineSnapshot(`
