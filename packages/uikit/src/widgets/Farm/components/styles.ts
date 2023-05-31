ecbab50f5ab9dc2170a5b7023d1b39a4cadc2d05
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 17:24:28 +0700

    Update styles.ts
    コードにはフロントエンドのテキストとカラーに関連するコードが含まれています。
    
    color: ${({ amount, theme }) => (amount ? theme.colors.text : theme.colors.textDisabled)};という行は、金額のテキストの色を、金額が存在する場合はテーマの text カラーに、金額が存在しない場合テーマの textDisabled カラーに設定しています。
    
    display: flex;という行は、金額のテキストを flex コンテナとしてレイアウトしています。
    
    align-items: center;という行は、金額のテキストを flex コンテナの中央に配置しています。

--- packages/uikit/src/widgets/Farm/components/styles.ts
+++ packages/uikit/src/widgets/Farm/components/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const Amount = styled.span<{ amount: number }>`
   color: ${({ amount, theme }) => (amount ? theme.colors.text : theme.colors.textDisabled)};
   display: flex;
