99e80c05600d85e23288223dc01346c3503320ac
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 17:18:35 +0700

    Update styles.tsx
    コードにはフロントエンドのテキストとカラーに関係するコードが含まれています。
    
    StyledMenuItemコンポーネントのcolorプロパティは、メニューアイテムがアクティブでないときはtheme.colors.textSubtle、アクティブなときはtheme.colors.secondaryの値を受け取ります。これにより、メニューアイテムのテキストの色を制御できます。
    
    StyledMenuItemコンポーネントのbackgroundプロパティは、メニューアイテムがホバーされたときにtheme.colors.tertiaryの値を受け取ります。これにより、メニューアイテムがホバーされたときに背景色を制御できます。
    
    したがって、コードはメニューアイテムのテキストと色を制御するために使用できます。

--- packages/uikit/src/components/MenuItem/styles.tsx
+++ packages/uikit/src/components/MenuItem/styles.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { StyledMenuItemProps } from "./types";
 
+
 export const StyledMenuItemContainer = styled.div<StyledMenuItemProps>`
   position: relative;
 
