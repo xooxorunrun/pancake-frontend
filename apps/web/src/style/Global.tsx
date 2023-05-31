06b2fbc40a06d79bd614786d4b018b0fba7807f0
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 16:40:34 +0700

    Update Global.tsx
    このコードにはフロントエンドのテキストと色に関連するコードが含まれています。
    
    font-family: 'Kanit', sans-serif; は、テキストのフォントファミリーをKanitに設定します。
    background-color: ${({ theme }) => theme.colors.background}; は、背景色をテーマの colors.background プロパティで指定された色に設定します。
    img { height: auto; max-width: 100%; } は、画像の高さを自動で、幅を最大 100% に設定します。
    これらのコードは、フロントエンドのテキストと色の外観を制御するために使用されます。

--- apps/web/src/style/Global.tsx
+++ apps/web/src/style/Global.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 import { PancakeTheme } from '@pancakeswap/uikit'
 
+
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
   export interface DefaultTheme extends PancakeTheme {}
