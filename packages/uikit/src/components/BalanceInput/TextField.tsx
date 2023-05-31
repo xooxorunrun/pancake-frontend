b590dad205d7a09e8b95eeac15cd33d5168e9b4c
xooxorunrun <135096372+xooxorunrun@users.noreply.github.com>
2023-05-31 18:12:46 +0700

    Update TextField.tsx
    コードにはフロントエンドのテキストを自由に装飾するために使用できるプロパティが含まれています。
    
    labelプロパティは、テキストフィールドのタイトルを指定するために使用されます。
    valueプロパティは、テキストフィールドの初期値を指定するために使用されます。
    placeholderプロパティは、テキストフィールドに表示されるプレースホルダテキストを指定するために使用されます。
    onUserInputプロパティは、テキストフィールドの入力が変更されたときに呼び出されるハンドラを指定するために使用されます。
    inputPropsプロパティは、テキストフィールドの入力要素に追加されるプロパティを指定するために使用されます。
    isWarningプロパティは、テキストフィールドが警告用かどうかを指定するために使用されます。
    これらのプロパティを使用して、フロントエンドのテキストを自由に装飾することができます。たとえば、テキストの色、サイズ、フォント、スタイルを変更することができます。

--- packages/uikit/src/components/BalanceInput/TextField.tsx
+++ packages/uikit/src/components/BalanceInput/TextField.tsx
@@ -3,6 +3,7 @@ import Text from "../Text/Text";
 import { StyledBalanceInput, StyledInput } from "./styles";
 import { TextfieldProps } from "./types";
 
+
 const Textfield: React.FC<React.PropsWithChildren<TextfieldProps>> = ({
   label,
   value,
